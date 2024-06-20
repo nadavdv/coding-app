f700d04cbe8a8887313ddca5796529ffe5132b93
nadavdv <nadavdv@gmail.com>
2024-06-20 20:40:09 +0300

    changed server provider

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import LobbyPage from "./components/LobbyPage.js";
 import CodeBlockPage from "./components/CodeBlockPage.js";
 
-export const socket = io("https://zooming-vibrancy-production.up.railway.app"); // Connect to backend server
+export const socket = io("https://api.render.com/deploy/srv-cpq6hb9u0jms738q6d8g?key=xFWhkZCvBVM"); // Connect to backend server
 
 function App() {
   return (
