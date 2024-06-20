3df43ebdfe9415f5eafce86a90866ed29b357c08
nadavdv <nadavdv@gmail.com>
2024-06-20 15:13:49 +0300

    update URL's

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // allow requests from the frontend
+    origin: "https://coding-app-production.up.railway.app/", // allow requests from the frontend
     methods: ["GET", "POST"],
   },
 });
